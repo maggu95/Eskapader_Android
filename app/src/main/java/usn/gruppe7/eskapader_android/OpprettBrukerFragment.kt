71a9529bf8d397afb9fb5f0f26d13c59ce9792bc
maggu95 <57000576+maggu95@users.noreply.github.com>
2021-09-16 13:14:18 +0200

    Lagde opprett bruker side og la til navigation controller

---
+++ app/src/main/java/usn/gruppe7/eskapader_android/OpprettBrukerFragment.kt
@@ -0,0 +1,24 @@
+package usn.gruppe7.eskapader_android
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import usn.gruppe7.eskapader_android.databinding.FragmentOpprettBrukerBinding
+
+
+class OpprettBrukerFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding = DataBindingUtil.inflate<FragmentOpprettBrukerBinding>(inflater,R.layout.fragment_opprett_bruker,container,false)
+        return binding.root
+    }
+
+
+}
