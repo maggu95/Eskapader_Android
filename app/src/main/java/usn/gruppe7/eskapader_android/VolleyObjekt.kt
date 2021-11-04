f16c3d97810e901dfd15d2f5ff90cdcdb20235f2
maggu95 <57000576+maggu95@users.noreply.github.com>
2021-11-04 11:03:05 +0100

    Laget start på en volley
    Denne må utfylles slik at den kan brukes

---
+++ app/src/main/java/usn/gruppe7/eskapader_android/VolleyObjekt.kt
@@ -0,0 +1,39 @@
+package usn.gruppe7.eskapader_android
+
+import android.content.Context
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
+
+class VolleyObjekt(val appContext: Context) : Volley() {
+
+
+
+    fun hentSpill(url: String) {
+        val queue = Volley.newRequestQueue(appContext)
+        val json = JsonArrayRequest(
+            Request.Method.GET, url, null,
+            {
+                response ->
+                val resultat = response.toString()
+                println("Resultat= $resultat")
+
+            },
+            {
+                error ->
+                println("Feil oppsto: $error")
+
+            })
+
+
+        queue.add(json)
+        println("La til forespørsel i kø")
+
+    }
+
+
+
+
+
+
+}
