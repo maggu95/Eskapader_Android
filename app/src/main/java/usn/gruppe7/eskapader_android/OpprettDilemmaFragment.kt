a28bb6ed9bee6300d23c566319796a57d1d526b6
boandree <bo.andree.larsen@gmail.com>
2021-11-11 13:28:32 +0100

    dilemma add arraylist

--- app/src/main/java/usn/gruppe7/eskapader_android/OpprettDilemmaFragment.kt
+++ app/src/main/java/usn/gruppe7/eskapader_android/OpprettDilemmaFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import usn.gruppe7.eskapader_android.databinding.FragmentHovedMenyBinding
@@ -19,7 +20,16 @@ class OpprettDilemmaFragment : Fragment() {
 
         val binding = DataBindingUtil.inflate<FragmentOpprettDilemmaBinding>(inflater,R.layout.fragment_opprett_dilemma,container,false)
 
+        val textList = ArrayList<EditText>()
+        textList.add(binding.dilemmaSpm)
+        textList.add(binding.dilemmaAlt1)
+        textList.add(binding.dilemmaAlt2)
 
+        binding.btLeggtilDilemma.setOnClickListener {
+            for (item in textList) {
+                item.text.clear()
+            }
+        }
 
         return binding.root;
     }
