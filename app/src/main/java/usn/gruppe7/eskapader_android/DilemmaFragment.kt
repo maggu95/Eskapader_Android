883b155e61647370bb143aca2d31cbc5aabf2775
boandree <bo.andree.larsen@gmail.com>
2021-11-15 12:33:52 +0100

    dilemma funksjonalitet (-stats)

--- app/src/main/java/usn/gruppe7/eskapader_android/DilemmaFragment.kt
+++ app/src/main/java/usn/gruppe7/eskapader_android/DilemmaFragment.kt
@@ -2,8 +2,6 @@ package usn.gruppe7.eskapader_android
 
 import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.os.Debug
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -17,10 +15,9 @@ import usn.gruppe7.eskapader_android.databinding.FragmentDilemmaBinding
 
 class DilemmaFragment : Fragment() {
 
-    lateinit var valgtShape: Drawable
-    lateinit var  defaultShape : Drawable
-    lateinit var feilShape : Drawable
+    lateinit var  normalShape : Drawable
     lateinit var korrektShape: Drawable
+    private var valgtDilemma : Boolean = true
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,10 +32,9 @@ class DilemmaFragment : Fragment() {
 
 
         //Shapes
-        valgtShape = context?.let { getDrawable(it,R.drawable.rounded_borders_musikkquiz) }!!
-        defaultShape = context?.let { getDrawable(it,R.drawable.rounded_corner_view) }!!
-        feilShape =  context?.let { getDrawable(it,R.drawable.rounded_borders_musikkquiz_feil) }!!
-        korrektShape = context?.let { getDrawable(it,R.drawable.rounded_borders_musikkquiz_korrekt) }!!
+        normalShape = context?.let { getDrawable(it,R.drawable.rounded_borders_dilemma2) }!!
+        korrektShape = context?.let { getDrawable(it,R.drawable.rounded_borders_dilemma) }!!
+
 
         //Henter listen med dillemma
         val dilemmaListe =  arguments?.getParcelableArrayList<Dilemma>("liste") as ArrayList<Dilemma>
@@ -47,24 +43,48 @@ class DilemmaFragment : Fragment() {
         binding.dillAlt2.text = dilemmaListe.get(currDilemma).alternativ[1]
 
 
-
-
         //Du valgte alternativ 1
         binding.dillAlt1.setOnClickListener{
-
+            if (valgtDilemma == false)
+                binding.dillAlt1.background = normalShape
+            else
+                binding.dillAlt1.background = korrektShape
+                binding.btNesteDilemma.visibility = View.VISIBLE
+                valgtDilemma = true
         }
 
         //Du valgte alternativ 2
         binding.dillAlt2.setOnClickListener{
+            if (valgtDilemma == false)
+                binding.dillAlt2.background = normalShape
+            else
+                binding.dillAlt2.background = korrektShape
+            binding.btNesteDilemma.visibility = View.VISIBLE
+            valgtDilemma = true
+
 
         }
 
         //Neste dilemma btn
         binding.btNesteDilemma.setOnClickListener {
 
+            if(currDilemma == 4) {
+                val dilemmaFerdigFragment = DilemmaFerdigFragment.newInstance(stats = "")
+                var fr = getFragmentManager()?.beginTransaction()
+                fr?.replace(R.id.dilemma_Container, dilemmaFerdigFragment)
+                fr?.commit()
+            }
+
+            if (valgtDilemma == true) {
+                binding.btNesteDilemma.text = "Neste"
+                currDilemma++
+                binding.dillAlt1.text = dilemmaListe[currDilemma].alternativ[0]
+                binding.dillAlt2.text = dilemmaListe[currDilemma].alternativ[1]
+                binding.dillAlt1.background = normalShape
+                binding.dillAlt2.background = normalShape
+                binding.btNesteDilemma.visibility = View.INVISIBLE
+            }
         }
-
-
 
 
         // Returnerer binding for view
@@ -72,7 +92,6 @@ class DilemmaFragment : Fragment() {
     }
 
     companion object {
-        @JvmStatic
         fun newInstance(dilemmaListe : ArrayList<Dilemma>) =
             DilemmaFragment().apply {
                 arguments = Bundle().apply {
@@ -81,3 +100,4 @@ class DilemmaFragment : Fragment() {
             }
     }
 }
+
